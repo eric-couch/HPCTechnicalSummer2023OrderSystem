555df2927633df514b8615fc5d7fbfef61923dc8
Eric Couch <ericcouch@yahoo.com>
2023-08-01 14:17:33 -0500

    added dbcontext and connection string

---
+++ HPCTechnicalSummer2023OrderSystem/Data/StoreContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HPCTechnicalSummer2023OrderSystem.Models;
+
+namespace HPCTechnicalSummer2023OrderSystem.Data;
+
+internal class StoreContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Sourse=(localdb)\\mssqllocaldb;Initial Catalog=HpcTechnicalStore;Integrated Security=True;");
+    }
+
+    // override the onModelCreate to seed data
+}
+
+
+
+
